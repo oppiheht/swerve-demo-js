6514c74486b11603d01cbb03df9833555343756b
Brandon Bussjaeger <oppiheht@gmail.com>
2017-09-15 20:23:26 -0700

    Added gamepad support

--- swerve.js
+++ swerve.js
@@ -1,23 +1,43 @@
 window.onload = function() {
 
-
   let keysPressed = {};
   onkeydown = onkeyup = function(e) {
     keysPressed[e.keyCode] = e.type == 'keydown';
   }
 
   setInterval(driveRobot, 33);
+  let gamepadCheckInterval = setInterval(detectGamepad, 100);
 
-  let TURN_SPEED = 5;
+  let gamepadIndex = null;
+  let TURN_SPEED = 10;
   let DRIVE_SPEED = 10;
   let DEGREES_TO_RADS = Math.PI / 180;
-  let fieldCentricControls = false;
+  let fieldCentricControls = true;
   let robot = document.getElementById('robot');
   robot.style.top = '100px';
   robot.style.left = '100px';
   robot.style.transform = 'rotate(0)';
 
+  function driveWithGamepad() {
+    let gamepad = navigator.getGamepads()[gamepadIndex]
+    console.log(gamepad.buttons[0].value);
+    let degrees = parseFloat(robot.style.transform.substr(7));
+    let top = parseFloat(robot.style.top);
+    let left = parseFloat(robot.style.left);
+
+    degrees += gamepad.axes[2].toFixed(1) * TURN_SPEED;
+    top += gamepad.axes[1].toFixed(1) * DRIVE_SPEED;
+    left += gamepad.axes[0].toFixed(1)* DRIVE_SPEED;
+
+    robot.style.transform = 'rotate('+degrees+'deg)';
+    robot.style.left = left + 'px';
+    robot.style.top = top + 'px';
+  }
+
   function driveRobot() {
+    if (gamepadIndex != null) {
+      driveWithGamepad();
+    }
     let degrees = parseFloat(robot.style.transform.substr(7));
     let top = parseFloat(robot.style.top);
     let left = parseFloat(robot.style.left);
@@ -71,6 +91,16 @@ window.onload = function() {
     robot.style.transform = 'rotate('+degrees+'deg)';
     robot.style.left = left + 'px';
     robot.style.top = top + 'px';
+  }
+
+  function detectGamepad() {
+    let g = navigator.getGamepads();
+    for (let i = 0; i < g.length; i++) {
+      if (g[i] && g[i].buttons[0].value == 1) {
+        gamepadIndex = i;
+        clearInterval(gamepadCheckInterval);
+      }
+    }
   }
 
 };
